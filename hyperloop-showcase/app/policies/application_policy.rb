cd0a4738cd9523c4e445c92335a105f1bea4ecc0
fzingg <fredericzingg@protonmail.com>
2017-02-18 18:52:01 +0000

    Update policy.rb

--- hyperloop-showcase/app/policies/application_policy.rb
+++ hyperloop-showcase/app/policies/application_policy.rb
@@ -10,4 +10,4 @@ class ApplicationPolicy
   regulate_all_broadcasts { |policy| policy.send_all }
   # Allow all changes to public models
   allow_change(to: :all, on: [:create, :update, :destroy]) { true }
-end if Rails.env.development?
+end
